9e353101c77dc2831f3c32973f65ecf40369648f
Mohan Raj <praem1990@gmail.com>
2021-05-14 17:21:14 +0530

    Split modules

--- lua/phpcs/init.lua
+++ lua/phpcs/init.lua
@@ -4,84 +4,38 @@ local phpcs_path = "/home/praem90/.config/composer/vendor/bin/phpcs"
 local phpcbf_path =	"/home/praem90/.config/composer/vendor/bin/phpcbf"
 local phpcs_standard = "PSR2"
 
-local finders = require('telescope.finders')
-local make_entry = require('telescope.make_entry')
-local pickers = require('telescope.pickers')
-local conf = require('telescope.config').values
+local Picker = require('picker')
+local lutils = require('utils')
 
-
-function backticks_table(cmd)
-	local tab = {}
-	local pipe = assert(io.popen(cmd),
-		"backticks_table(" .. cmd .. ") failed.")
-	local line = pipe:read("*line")
-	while line do
-		table.insert(tab, line)
-		line = pipe:read("*line")
-	end
-	return tab
-end
-
+-- Config Variables
+M.phpcs_path = vim.g.nvim_phpcs_config_phpcs_path or phpcs_path
+M.phpcbf_path = vim.g.nvim_phpcs_config_phpcbf_paths or phpcbf_path
 
 M.cs = function ()
 	local cmd = {
-		phpcs_path,
+		M.phpcs_path,
 		"--report=emacs",
 		"--standard=" .. phpcs_standard,
 		vim.fn.expand('%')
 	}
 
-    showPicker(cmd)
+    Picker.show(cmd)
 end
 
 
 M.cbf = function ()
-	local phpcs = {
-		phpcbf_path,
+	if vim.g.disable_cbf then
+		return
+	end
+
+	local cmd = {
+		M.phpcbf_path,
 		"--standard=" .. phpcs_standard,
 		vim.fn.expand('%')
 	}
-	local output = backticks_table(table.concat(phpcs, " "))
+
+	lutils.backticks_table(table.concat(cmd, " "))
 	vim.cmd('e')
-end
-
-function showPicker(find_command, opts)
-    opts = opts or {};
-    opts.entry_maker = gen_from_output(opts)
-      opts.cwd = opts.cwd and vim.fn.expand(opts.cwd) or vim.loop.cwd()
-    pickers.new(opts, {
-        prompt_title = 'PHPCS',
-        finder = finders.new_oneshot_job(
-          find_command,
-          opts
-        ),
-        previewer = conf.grep_previewer(opts),
-        sorter = conf.file_sorter(opts),
-    }):find()
-end
-
-function gen_from_output(_)
-  return function(line)
-    local cursor_position = split(line, ':')
-
-    return {
-      value = line,
-      ordinal = line,
-      display = line,
-      lnum = cursor_position[2],
-      col = cursor_position[3],
-      start = cursor_position[2],
-	  filename = vim.api.nvim_buf_get_name(cursor_position[1])
-    }
-  end
-end
-
-function split(s, delimiter)
-    result = {};
-    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-        table.insert(result, match);
-    end
-    return result;
 end
 
 return M
---
+++ lua/phpcs/utils.lua
@@ -0,0 +1,19 @@
+function backticks_table(cmd)
+	local tab = {}
+	local pipe = assert(io.popen(cmd),
+		"backticks_table(" .. cmd .. ") failed.")
+	local line = pipe:read("*line")
+	while line do
+		table.insert(tab, line)
+		line = pipe:read("*line")
+	end
+	return tab
+end
+
+function split(s, delimiter)
+    result = {};
+    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
