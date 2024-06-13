3adb8f85a99253683421f083163452521e5f85a3
Mohan Raj <praem1990@gmail.com>
2024-06-13 15:06:26 +0530

    Clean up

--- lua/phpcs/init.lua
+++ lua/phpcs/init.lua
@@ -1,6 +1,4 @@
 local M = {};
-local json = require('phpcs.json')
-
 local root = vim.loop.cwd()
 local phpcs_path = "$HOME/.config/composer/vendor/bin/phpcs"
 local phpcbf_path = "$HOME/.config/composer/vendor/bin/phpcbf"
@@ -15,6 +13,7 @@ M.phpcbf_path = vim.g.nvim_phpcs_config_phpcbf_path or phpcbf_path
 M.phpcs_standard = vim.g.nvim_phpcs_config_phpcs_standard or phpcs_standard
 M.last_stderr = ''
 M.last_stdout = ''
+M.nvim_namespace = nil
 
 M.detect_local_paths = function ()
     if (lutils.file_exists('phpcs.xml')) then
@@ -28,23 +27,31 @@ M.detect_local_paths = function ()
     if (lutils.file_exists('vendor/bin/phpcbf')) then
         M.phpcbf_path = root .. '/vendor/bin/phpcbf'
     end
+
+    M.nvim_namespace = vim.api.nvim_create_namespace("phpcs")
 end
 
 M.cs = function ()
 	local bufnr = vim.api.nvim_get_current_buf()
+
+    local report_file = os.tmpname();
 
 	local opts = {
 		command = M.phpcs_path,
   		args = {
     		"--stdin-path=" .. vim.api.nvim_buf_get_name(bufnr),
 			"--report=json",
+            "--report-file=" .. report_file,
 			"--standard=" .. M.phpcs_standard,
     		"-"
   		},
       	writer = vim.api.nvim_buf_get_lines(bufnr, 0, -1, true),
-  		on_exit = vim.schedule_wrap(function(j)
-            print(vim.inspect(j:result()))
-  			M.publish_diagnostic(j:result(), bufnr)
+  		on_exit = vim.schedule_wrap(function()
+            local file = io.open(report_file, "r")
+            if file ~= nil then
+                local content = file:read("*a")
+                M.publish_diagnostic(content, bufnr)
+            end
   		end),
   	}
 
@@ -72,59 +79,48 @@ M.cbf = function (new_opts)
 	local opts = {
 		command = M.phpcbf_path,
   		args = {
-    		"--stdin-path=" .. vim.api.nvim_buf_get_name(new_opts.bufnr),
 			"--standard=" .. M.phpcs_standard,
-    		"-"
+            vim.api.nvim_buf_get_name(new_opts.bufnr)
   		},
-      	writer = vim.api.nvim_buf_get_lines(new_opts.bufnr, 0, -1, true),
+  		on_exit = vim.schedule_wrap(function()
+            vim.diagnostic.set(M.nvim_namespace, new_opts.bufnr, {})
+            vim.cmd("e")
+  		end),
       	cwd = vim.fn.getcwd(),
   	}
 
-  	local results, status = Job:new(opts):sync(new_opts.timeout or 1000)
-
-  	status = tonumber(status)
-
-  	if status <= 1 then
-  		vim.api.nvim_buf_set_lines(new_opts.bufnr, 0, -1, true, results)
-    	M.cs()
-    else
-    	error("Failed to run code beautifier")
-	end
+  	Job:new(opts):start()
 end
 
 M.publish_diagnostic = function (results, bufnr)
-	local method = 'textDocument/publishDiagnostics';
-
 	bufnr = bufnr or vim.api.nvim_get_current_buf()
 
-    local diagnostics = parse_json(table.concat(results), bufnr)
+    local diagnostics = parse_json(results, bufnr)
 
-    local ns = vim.api.nvim_create_namespace("phpcbf")
-    vim.diagnostic.set(ns, bufnr, diagnostics)
+    vim.diagnostic.set(M.nvim_namespace, bufnr, diagnostics)
 end
 
 function parse_json(encoded, bufnr)
-    local decoded = json.decode(encoded)
+    local decoded = vim.json.decode(encoded)
     local diagnostics = {}
-    local uri = vim.api.nvim_buf_get_name(bufnr);
+    local uri = vim.fn.bufname(bufnr);
 
-	local error_codes = {
-		['error'] = vim.lsp.protocol.DiagnosticSeverity.Error,
-		warning = vim.lsp.protocol.DiagnosticSeverity.Warning,
-	}
+    local error_codes = {
+        ['error'] = vim.lsp.protocol.DiagnosticSeverity.Error,
+        warning = vim.lsp.protocol.DiagnosticSeverity.Warning,
+    }
 
-	if not decoded.files[uri] then
-		return diagnostics
-	end
+    if not decoded.files[uri] then
+        return diagnostics
+    end
 
-	for _, message in ipairs(decoded.files[uri].messages) do
+    for _, message in ipairs(decoded.files[uri].messages) do
         table.insert(diagnostics, {
             severity = error_codes[string.lower(message.type)],
             lnum	 = tonumber(message.line) -1,
             col	 = tonumber(message.column) -1,
             message = message.message
         })
-		::continue::
     end
 
     return diagnostics
